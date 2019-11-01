ee8f0591d42a53f9dca16065e9298d96ae7b6779
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-11-01 13:29:29 -0400

    added styling to better show off dark mode functionality and display cards in a readable way

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
         <nav className="navbar">
             <h1 data-testid="navbar-header">Meet The Players!</h1>
             <div className="dark-mode__toggle">
-            <button data-testid="darkModeButton" onClick={toggleDark} className={darkMode ? 'toggle toggled' : 'toggle'}>Change Light Mode</button>
+            <button data-testid="darkModeButton" onClick={toggleDark} className={darkMode ? 'toggle toggled' : 'toggle'}>Change Mode</button>
             </div>
         </nav>
     );
